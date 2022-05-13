9f80ea3d5ccb798df524152c7c0f74e5ea97e571
Lior Matsliah <liormatsliah1985@gmail.com>
2022-05-13 19:40:23 +0300

    ctor -> Orders

--- User.cs
+++ User.cs
@@ -9,5 +9,10 @@ namespace BitCoinManagerModels
         public string Email { get; set; }
         public string Password { get; set; }
         public List<Order> Orders { get; set; }
+
+        public User()
+        {
+            Orders = new List<Order>();
+        }
     }
 }
