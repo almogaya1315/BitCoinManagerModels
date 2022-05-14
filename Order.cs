7391b81ebb98c29dd428badab15e5e67c6f987c3
Lior Matsliah <liormatsliah1985@gmail.com>
2022-05-14 14:14:38 +0300

    OrderId

--- Order.cs
+++ Order.cs
@@ -6,6 +6,7 @@ namespace BitCoinManagerModels
 {
     public class Order
     {
+        public int Id { get; set; }
         public double Amount { get; set; }
         public double Price { get; set; }
         public OrderOperation Operation { get; set; }
